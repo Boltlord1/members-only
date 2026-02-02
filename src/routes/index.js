6bff78cd88931bdb4ceea6106e88259da30d2e1c
Boltlord1 <boltlord1@gmail.com>
2026-02-02 05:10:17 +0500

    Created forms to login and signup

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,5 +3,7 @@ import { Router } from 'express'
 const router = Router()
 
 router.get('/', (req, res) => res.render('index'))
+router.get('/login', (req, res) => res.render('login'))
+router.get('/signup', (req, res) => res.render('signup'))
 
 export default router
